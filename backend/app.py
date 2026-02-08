8b3981a9dbf541db4f2881657c4939fe973b9d07
Kewal-Deshpande <animating.kewal@gmail.com>
2026-02-08 20:05:24 +0530

    fixed Railway crash by using dynamic port

--- backend/app.py
+++ backend/app.py
@@ -202,4 +202,7 @@ if __name__ == '__main__':
 
     print("TouristBuddy backend running...")
 
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(host="0.0.0.0", port=port)
